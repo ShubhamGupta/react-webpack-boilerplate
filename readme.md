844a531ee0482baa6de67c5006f62500d70af6d7
Shubham Gupta <shubham@vinsol.com>
2016-06-24 13:25:19 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+Ref URL: https://www.codementor.io/reactjs/tutorial/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack
+
+Terminal commands:
+
+Runs the webpack in the development mode and generates the bundle.js file and its associated map file bundle.js.map in the src/client/public directory
+./node_modules/.bin/webpack -d --watch
+
+To run the server
+webpack-dev-server
+
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
